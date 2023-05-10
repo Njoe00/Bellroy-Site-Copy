2f039bb4eee2c499df29ee3212f6135ae10d3308
Njoe00 <nick.james.joe@gmail.com>
2023-05-09 19:11:08 -0700

    fixing sizing and positons of header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,31 +5,38 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="bg-white p-10 flex justify-evenly">
-      <a>
-        <img src="pages/bellroy9718.jpg" alt='hello'/>
+    <main>
+      <a className="bg-white p-10 flex gap-2 font-normal space-x-14 text-sm">
+          <img src="pages/bellroy9718.jpg" alt='hello'/>
+        <div>
+          Bags
+        </div>
+        <div>
+          Wallets
+        </div>
+        <div>
+          Accessories
+        </div>
+        <div>
+          Tech
+        </div>
+        <div>
+          Travel
+        </div>
+        <div>
+          Collects
+        </div>
+        <div>
+          About Us
+        </div>
+          <div className='flex justify-self-end'>
+            Help
+          </div>
+          <div>
+            Find In-Store
+        </div>
       </a>
-      <div>
-        Bags
-      </div>
-      <div>
-        Wallets
-      </div>
-      <div>
-        Accessories
-      </div>
-      <div>
-        Tech
-      </div>
-      <div>
-        Travel
-      </div>
-      <div>
-        Collects
-      </div>
-      <div>
-        About
-      </div>
     </main>
+    
   )
 }
