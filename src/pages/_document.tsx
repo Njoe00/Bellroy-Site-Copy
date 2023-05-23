31509ea47f2a43eaac55c9e429a75760cf3e27af
Njoe00 <nick.james.joe@gmail.com>
2023-05-22 18:21:39 -0700

    adding sliding and text info counter

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Image from 'next/image'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='h-screen bg-background-main'>
+      <body >
         <Main />
         <NextScript />
-        <div>
-          <h1 className='font-serif flex text-white text-8xl p-16 justify-center'>Slim Your Wallet</h1>
-        </div>
-        <div>
-          <p className='flex text-base p-0 font-bold justify-center'>Move the slider to compare wallets</p>
-        </div>
-        <div>
-          <img src='./pages/syw-compare.avif' alt='hello'/>
-        </div>
       </body>
     </Html>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,33 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [sliderValue, setSliderValue] = useState("0");
+  let textValue;
+  if (sliderValue === "9") {
+    textValue = 'Plus Cash';
+  } 
+  if (sliderValue === "1") {
+    textValue = "1 Card";
+  } else {
+    textValue = `${sliderValue} Cards`;
+  }
   return (
     <header className="bg-white">
       <span className="place-content-end p-1 text-xs right-6 absolute text-gray-500 font-normal">
         Free shipping available 🇨🇦
       </span>
-      <div className="bg-white p-5 flex justify-between gap-2 font-normal space-x-14 text-sm items-center" >
+      <div className="bg-white p-5 flex justify-between gap-2 font-normal space-x-14 text-sm items-center h-28" >
         <div className="gap-2 font-normal space-x-14 items-center flex">
-          <img src="pages/logo.png"/>
+          <Image
+            src='/../public/logo.png'
+            width={120}
+            height={1}
+            alt='test'
+          />
           <a href="/#">Bags</a>
           <a href="/#">Wallets</a>
           <a href="/#">Accessories</a>
@@ -26,6 +42,32 @@ export default function Home() {
           <img src='src/pages/email.png' alt='mail'/>
         </div>
       </div>
+      <div className='h-screen bg-background-main'>
+          <h1 className='font-serif flex text-white text-8xl p-16 justify-center'>Slim Your Wallet</h1>
+        </div>
+        <div>
+          <p className='flex text-base p-0 font-bold justify-center'>Move the slider to compare wallets</p>
+        </div>
+        <div className='block mr-auto ml-auto w-28'>
+          <Image 
+          src='/syw-compare.avif'
+          width={500} 
+          height={800}
+          quality={100}
+          alt='alt'
+          />
+          <div className='flex flex-col space-y-2 p-2 w-80'>
+            <input type='range' className='w-full' min="0" max="9" step="1" value={sliderValue} 
+              onChange={(event) => {
+                setSliderValue(event.target.value)
+                console.log(event.target.value, ' this is value')
+              }} 
+            />
+            <ul className="flex justify-between w-full px-2.5"> 
+              <li className="flex justify-center relative"><span className="absolute">{textValue}</span></li>
+            </ul>
+          </div>
+        </div>
     </header>
     
   )
