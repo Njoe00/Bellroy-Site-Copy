9d90cf8beaee783aff2f5368e1e08873e2cfd9a7
Njoe00 <nick.james.joe@gmail.com>
2023-06-13 12:28:07 -0700

    adding dual slidered with new div with text at bottom and readjust position of button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,22 +25,21 @@ export default function Home() {
   const slides = [
     {
       url: "https://bellroy-cms-images.imgix.net/3954/slider-image01.jpg",
-      text: "Made from premium, enviormentally certified leather, that only gets better with age..."
+      text: "Made from premium, enviormentally certified leather, that only gets better with age...",
     },
     {
       url: "https://bellroy-cms-images.imgix.net/4413/slider-image02.jpg",
-      text: "and fewer of leather, to eliminate bulk from the start..."
+      text: "and fewer of leather, to eliminate bulk from the start...",
     },
     {
       url: "https://bellroy-cms-images.imgix.net/4414/slider-image03.jpg",
-      text: "their clever design features help keep them slim, even when they're full…"
+      text: "their clever design features help keep them slim, even when they're full…",
     },
     {
       url: "https://bellroy-cms-images.imgix.net/4425/slider-image04.jpg",
-      text: "so they'll be with you from Day One to Day 1000, and beyond."
+      text: "so they'll be with you from Day One to Day 1000, and beyond.",
     },
   ];
-
 
   const [currentIndex, setCurrentIndex] = useState(1);
 
@@ -323,23 +322,40 @@ export default function Home() {
       </div>
 
       <div className="relative flex flex-col py-28 justify-center bg-gray-100 items-center">
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
-        >
+        <div className="flex flex-row space-x-4 drop-shadow-lg">
+        <div>
+            <div
+              style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+              className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
+            >
+              </div>
+              <div className="flex flex-col bottom-4 items-center text-xs">
+                <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
+                  {`${slides[currentIndex].text}`}
+                </div>
+            </div>
+          </div>
+          <div>
+            <div
+              style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+              className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
+            >
+              </div>
+              <div className="flex flex-col bottom-4 items-center text-xs">
+                <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
+                  {`${slides[currentIndex].text}`}
+                </div>
+            </div>
+          </div>
+        
         </div>
-        <div className="absolute top-[50%] -translate-x-[-675px] translate-y-[-10%] left-2 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
+        <div className="absolute top-[50%] -translate-x-[-500px] translate-y-[-10%] left-2 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
           <BsChevronCompactLeft onClick={prevSlide} size="40" />
         </div>
-        <div className="absolute top-[50%] -translate-x-[675px] translate-y-[-20%] right-1 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
+        <div className="absolute top-[50%] -translate-x-[500px] translate-y-[-20%] right-1 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size="40" />
         </div>
-          <div
-            className="flex flex-col bottom-4 items-center text-xs">
-              <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
-                {`${slides[currentIndex].text}`}
-              </div>
-          </div>            
+
         <div className="flex absolute justify-center bottom-4 items-center">
           {slides.map((slide, slideIndex) => (
             <div
