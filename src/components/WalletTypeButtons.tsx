212a55d1c777426c01460ef797b31b686e4ce9f2
Njoe00 <nick.james.joe@gmail.com>
2023-07-15 23:09:42 -0700

    adding oringal type cotnent to typescript

--- src/components/WalletTypeButtons.tsx
+++ src/components/WalletTypeButtons.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import { BsChevronCompactDown } from "react-icons/bs";
 import WalletTypeButton from "./WalletTypeButton";
+
+
 
 export default function WalletTypeButtons({
   imagesToShow,
