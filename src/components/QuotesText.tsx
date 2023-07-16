f54c8e9429c3f5aa19c4b970ea58ba45382b8071
Njoe00 <nick.james.joe@gmail.com>
2023-07-16 12:13:23 -0700

    add highlighted button state to change which butotn is highlighted along with styling to change from gray to white when clicked

--- src/components/QuotesText.tsx
+++ src/components/QuotesText.tsx
@@ -7,24 +7,44 @@ type quotes = [
   }
 ];
 
+type SetState = [
+  setQuotesText: (value: string) => void,
+  setButtonHighLighted: (value: number) => void
+];
+
 export default function QuotesText({
   Quotes,
+  buttonHighlighted,
   setQuotesText,
+  setButtonHighLighted,
+  index,
 }: {
   Quotes: quotes;
-  setQuotesText: (val: string) => void;
+  buttonHighLighted: number;
+  setQuotesText: SetState[0];
+  setButtonHighLighted: SetState[1];
+  index: number;
 }) {
   return (
     <div className="flex-row flex space-x-6">
       {Quotes.map((text, index) => {
         return (
-          <div
-            className="text-xl"
-            key={index}
-            onClick={() => setQuotesText(text.quote)}
-          >
-            {text.title}
-          </div>
+          <>
+            <div
+              className={
+                buttonHighlighted !== 0
+                  ? "hover:text-white text-white text-xl"
+                  : "text-gray-400 hover:text-white text-xl"
+              }
+              key={index}
+              onClick={() => {
+                setQuotesText(text.quote);
+                setButtonHighLighted(index);
+              }}
+            >
+              {text.title}
+            </div>
+          </>
         );
       })}
     </div>
