308effdfd9ae169715a73a10417760aa221f8e0a
Njoe00 <nick.james.joe@gmail.com>
2023-07-16 00:17:54 -0700

    adding map and passing Quotes data object as props along with TypeScript Type object

--- src/components/QuotesText.tsx
+++ src/components/QuotesText.tsx
@@ -1,30 +1,23 @@
 import React from "react";
 
+type quotes = [
+  {
+    title: string;
+    quote: string;
+  }
+];
 
-export default function QuotesText() {
-    return (
+export default function QuotesText({ Quotes }: { Quotes: quotes }) {
+  return (
+    <>
+      {Quotes.map((text, index) => {
+        return (
+          <div className="text-xl" key={index}>
+            {text.title}
+          </div>
+        );
+      })}
+    </>
+  );
+}
 
-    );
-};
-
-
-const Quotes = [
-    {
-        title: "GEAR PATROL",
-        quote: "“Bellroy can take a backbreaker of a wallet and make it smaller than a deck of cards.”"
-    },
-    {
-        title: "MENS JOURNAL",
-        quote: "“Seriously slim wallets and passport holders.”"
-    },
-    {
-        title: "THE HUFFINGTON POST",
-        quote: "Bellroy is on a mission to slim down the way you carry your everyday essentials, by making it functional yet elegant."
-    },
-    {
-        title: "THE HUFFINGTON POST",
-        quote: "“Honestly, these wallets are near perfect.”"
-    }
-    
-
-]
