4a7844d27cef7188405066aab7fc67d64b715d13
Njoe00 <nick.james.joe@gmail.com>
2023-08-09 14:36:35 -0700

    state to manage when the input tage is focused on what the button changes colour and when clicked off will change colour

--- src/components/NewsLetterSignUp.tsx
+++ src/components/NewsLetterSignUp.tsx
@@ -1,22 +1,42 @@
-import react from "react";
+import React, { useState } from "react";
 
 export default function NewsLetterSignUp() {
+  const [buttonColour, setButtonColour] = useState(false);
+
+  const HandleButtonColour = () => {
+    setButtonColour(!buttonColour);
+  };
+
   return (
-    <div className="px-5 py-3.5 flex flex-col bg-background-news-letter-colour items-center z-50">
+    <div className="px-5 py-3.5 flex flex-col bg-background-news-letter-colour items-center">
       <h2 className="text-orange-600 text-center text-2xl font-serif flex justify-center">
         Get exclusive access to new products, deals & surprise treats.
       </h2>
-      <div className="w-[452px] h-10 justify-center items-center flex flex-row bg-white rounded-sm drop-shadow-md">
+      <div className="w-[452px] h-10 justify-center items-center flex flex-row bg-white rounded-md drop-shadow-lg m-4">
         <input
-          className="w-[381.94px] h-8 flex border-white text-sm font-serif"
+          className="w-[381.94px] h-8 flex border-none focus:outline-none outline-none text- font-serif"
           type="email"
           placeholder="Enter your email register"
+          onFocus={HandleButtonColour}
+          onBlur={HandleButtonColour}
         />
         <div className="flex justify-end items-center">
-          <button className="z-10 flex h-8 w-16 items-center text-center justify-center bg-gray-400 text-white rounded-md">
-            Sumbit
+          <button
+            className={`z-50 flex h-8 w-16 items-center text-center text-xs justify-center font-sans text-white rounded-md ${
+              buttonColour ? `bg-orange-600` : `bg-gray-300`
+            }`}
+          >
+            SUMBIT
           </button>
         </div>
+      </div>
+      <div className="w-[452px] text-xs text-center text-gray-400">
+        You are signing up to receive product updates and newsletters. By
+        signing up, you are consenting to our
+        <span className="whitespace-normal cursor-pointer text-orange-600 underline">
+          privacy policy
+        </span>
+        but you can opt out at any time.
       </div>
     </div>
   );
