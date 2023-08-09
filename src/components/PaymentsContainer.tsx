2fc4e894a3bfbf1da51463dd386bb57222c4abef
Njoe00 <nick.james.joe@gmail.com>
2023-08-09 10:53:14 -0700

    applying eslint

--- src/components/PaymentsContainer.tsx
+++ src/components/PaymentsContainer.tsx
@@ -9,33 +9,39 @@ export default function PaymentsContainer() {
       </div>
       <div className="flex justify-center flex-row items-center space-x-24">
         {PaymentIcons.map((icon, index) => (
-          <Image className="flex items-center"  src={icon.icon} height={icon.height} width={icon.width} alt="test" key={index} />
+          <Image
+            className="flex items-center"
+            src={icon.icon}
+            height={icon.height}
+            width={icon.width}
+            alt="test"
+            key={index}
+          />
         ))}
       </div>
     </div>
   );
 }
-7
+7;
 const PaymentIcons = [
   {
     icon: "/payment-icons-visa.avif",
     width: 64.63,
-    height: 22
+    height: 22,
   },
   {
     icon: "/payment-icons-mastercard-2.avif",
     width: 51.42,
-    height: 40
-
+    height: 40,
   },
   {
     icon: "/payment-icons-paypal.avif",
     width: 81.7,
-    height: 22
+    height: 22,
   },
   {
     icon: "/payment-icons-applepay.avif",
     width: 51.69,
-    height: 22
+    height: 22,
   },
 ];
