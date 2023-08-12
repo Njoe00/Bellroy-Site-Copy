86a21b64456bdf40c609e480b108f40f5de7be53
Njoe00 <nick.james.joe@gmail.com>
2023-08-11 19:54:10 -0700

    reworking data strucutre to an object with key pair value arrays and passing the header from the parent map to the child map as a key to access the arrays and returning the content

--- src/components/SiteMapFooter.tsx
+++ src/components/SiteMapFooter.tsx
@@ -5,13 +5,13 @@ export default function SiteMapFooter() {
     <div className="bg-background-site-footer-colour w-full h-[400px] flex flex-row justify-center">
       {sectionTitle.map((title, index) => (
         <ul
-          className="flex-column break-normal whitespace-pre text-white table-caption"
+          className="flex-column break-normal p-2 whitespace-pre text-white table-caption"
           key={index}
         >
           {title}
-          {subSection.map((section, index) => (
-            <li className="flex whitespace-pre-wrap w-56 h-6">
-              {section[title]}
+          {subSection[title].map((section, index) => (
+            <li className=" text-white flex whitespace-pre-wrap w-56 h-6">
+              {section}
             </li>
           ))}
         </ul>
@@ -21,64 +21,59 @@ export default function SiteMapFooter() {
 }
 
 const sectionTitle = ["HELP", "SHOP PRODUCTS", "SHOP COLLECTIONS", "ABOUT"];
-const subSection = [
-  {
-    HELP: [
-      "Customer Care",
-      "Shipping and Returns",
-      "3-Year Warranty",
-      "Contact Us",
-      "Terms & Conditions",
-      "Privacy Policy",
-      "Cookie Policy",
-    ],
-  },
-  {
-    "SHOP PRODUCTS": [
-      "Wallets",
-      "Passport Holders",
-      "RFID Protected",
-      "Bags",
-      "Phone Cases",
-      "Work Accessories",
-      "Travel",
-      "Pouches",
-      "Key Holders",
-      "Black Friday Exclusive",
-    ],
-  },
-  {
-    "SHOP COLLECTIONS": [
-      "New Releases",
-      "Bestsellers",
-      "Coming soon",
-      "Premium",
-      "Venture",
-      " For Tech Lovers",
-      "Small Bags",
-      "Recycled",
-      "Slim Your Wallet",
-      "Apex",
-      "Materials Hub",
-      "Value Sets",
-      "The Outlet",
-      "Travel",
-      "Lite Collection",
-    ],
-  },
-  {
-    ABOUT: [
-      "Our Story",
-      "Our Materials",
-      "Responsible Business",
-      "Ambassadors",
-      "Journal",
-      "Collaborations",
-      "Find In-Store",
-      "Affiliate Program",
-      "Corporate Gifting",
-      "Press",
-      "Careers",
-    ],
-  },
-];
+const subSection = {
+  HELP: [
+    "Customer Care",
+    "Shipping and Returns",
+    "3-Year Warranty",
+    "Contact Us",
+    "Terms & Conditions",
+    "Privacy Policy",
+    "Cookie Policy",
+  ],
+
+  "SHOP PRODUCTS": [
+    "Wallets",
+    "Passport Holders",
+    "RFID Protected",
+    "Bags",
+    "Phone Cases",
+    "Work Accessories",
+    "Travel",
+    "Pouches",
+    "Key Holders",
+    "Black Friday Exclusive",
+  ],
+
+  "SHOP COLLECTIONS": [
+    "New Releases",
+    "Bestsellers",
+    "Coming soon",
+    "Premium",
+    "Venture",
+    " For Tech Lovers",
+    "Small Bags",
+    "Recycled",
+    "Slim Your Wallet",
+    "Apex",
+    "Materials Hub",
+    "Value Sets",
+    "The Outlet",
+    "Travel",
+    "Lite Collection",
+  ],
+
+  ABOUT: [
+    "Our Story",
+    "Our Materials",
+    "Responsible Business",
+    "Ambassadors",
+    "Journal",
+    "Collaborations",
+    "Find In-Store",
+    "Affiliate Program",
+    "Corporate Gifting",
+    "Press",
+    "Careers",
+  ],
+};
