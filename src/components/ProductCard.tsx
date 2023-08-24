ecdc59b3813962e9e615ea6a264762822c7bf4b0
Njoe00 <nick.james.joe@gmail.com>
2023-08-24 09:50:44 -0700

    extracting rotatation compotent and dymically rendering inside class

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -32,6 +32,10 @@ export default function ProductCard({ content }: { content: Content }) {
     setIsCardFlipped(!isCardFlipped);
   };
 
+  const rotateCardImage = isCardFlipped
+    ? "-rotateY(180deg)"
+    : "rotateY(180deg)";
+
   return (
     <div className=" group flex flex-col flex-wrap justify-center border-4 bg-gray-100 border-white">
       <div className="flex flex-row relative justify-center">
@@ -45,11 +49,7 @@ export default function ProductCard({ content }: { content: Content }) {
       <div className="flex flex-col w-[413.33px] h-[508px] items-center relative bg-gray-100">
         <div className="top-10 relative">
           <div
-            className={`${
-              isCardFlipped
-                ? "[transform:-rotateY(180deg)]"
-                : "[transform:rotateY(180deg)]"
-            } ease-in-out [transform-style-:preserve-3d] [backface-visblity:hidden] transition-all duration-1000`}
+            className={`[transform:${rotateCardImage}] ease-in-out [transform-style-:preserve-3d] [backface-visblity:hidden] transition-all duration-1000`}
           >
             <Image
               className="transition-all ease-in-out duration-300"
