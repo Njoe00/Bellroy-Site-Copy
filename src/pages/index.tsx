7636c2f24fcdb7b1cb6a2fd255aa17444011fd1f
Njoe00 <nick.james.joe@gmail.com>
2023-09-06 21:17:46 -0700

    adding right image card floating partially above image slider container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -153,57 +153,65 @@ export default function Home() {
           </p>
         </div>
       </div>
-
-      <div className="relative flex flex-col py-28 justify-center bg-gray-100 items-center z-40">
-        <div className="flex flex-row space-x-4 drop-shadow-lg">
-          <div>
-            <div
-              style={{
-                backgroundImage: `url(${slidesSetOne[currentIndex].url})`,
-              }}
-              className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
-            ></div>
-            <div className="flex flex-col bottom-4 items-center text-xs">
-              <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
-                {`${slidesSetOne[currentIndex].text}`}
+      <div className="inline z-10">
+        <Image
+          className="z-50 right-0 absolute max-w-[190px] flex -mt-40"
+          src="/card-Sleeves/business-cardsx2.avif"
+          height="397"
+          width="190"
+          alt="alt"
+        />
+        <div className="relative flex flex-col py-28 justify-center bg-gray-100 items-center z-40">
+          <div className="flex flex-row space-x-4 drop-shadow-lg">
+            <div>
+              <div
+                style={{
+                  backgroundImage: `url(${slidesSetOne[currentIndex].url})`,
+                }}
+                className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
+              ></div>
+              <div className="flex flex-col bottom-4 items-center text-xs">
+                <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
+                  {`${slidesSetOne[currentIndex].text}`}
+                </div>
+              </div>
+            </div>
+            <div>
+              <div
+                style={{
+                  backgroundImage: `url(${slidesSlidesSetTwo[currentIndex].url})`,
+                }}
+                className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
+              ></div>
+              <div className="flex flex-col bottom-4 items-center text-xs">
+                <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
+                  {`${slidesSlidesSetTwo[currentIndex].text}`}
+                </div>
               </div>
             </div>
           </div>
-          <div>
-            <div
-              style={{
-                backgroundImage: `url(${slidesSlidesSetTwo[currentIndex].url})`,
-              }}
-              className="bg-cover w-[365px] h-[280px] duration-500 bg-no-repeat flex q"
-            ></div>
-            <div className="flex flex-col bottom-4 items-center text-xs">
-              <div className="bg-white w-[365px] h-[94px] flex flex-col text-center justify-center p-4">
-                {`${slidesSlidesSetTwo[currentIndex].text}`}
-              </div>
-            </div>
+          <div className="absolute top-[50%] -translate-x-[-500px] translate-y-[-40%] left-2 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
+            <BsChevronCompactLeft onClick={prevSlide} size="40" />
           </div>
-        </div>
-        <div className="absolute top-[50%] -translate-x-[-500px] translate-y-[-40%] left-2 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
-          <BsChevronCompactLeft onClick={prevSlide} size="40" />
-        </div>
-        <div className="absolute top-[50%] -translate-x-[500px] translate-y-[-60%] right-1 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
-          <BsChevronCompactRight onClick={nextSlide} size="40" />
-        </div>
+          <div className="absolute top-[50%] -translate-x-[500px] translate-y-[-60%] right-1 text-2xl rounded-full p-2 text-gray-300 cursor-pointer">
+            <BsChevronCompactRight onClick={nextSlide} size="40" />
+          </div>
 
-        <div className="flex absolute space-x-2 justify-center bottom-4 items-center">
-          {slidesSetOne.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className="text-2xl cursor-pointer flex-row flex"
-            >
-              <a
-                className={`h-3 w-3 outline-gray-400 rounded-full outline-1 outline ${
-                  currentIndex === slideIndex ? " bg-gray-400" : ""
-                }`}
-              ></a>
-            </div>
-          ))}
+          <div className="flex absolute space-x-2 justify-center bottom-4 items-center">
+            {slidesSetOne.map((slide, slideIndex) => (
+              <div
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+                className="text-2xl cursor-pointer flex-row flex"
+              >
+                <a
+                  className={`h-3 w-3 outline-gray-400 rounded-full outline-1 outline ${
+                    currentIndex === slideIndex ? " bg-gray-400" : ""
+                  }`}
+                ></a>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
